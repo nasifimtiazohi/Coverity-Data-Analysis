dd9aeb65f909ee0628375f8564ba813752ceb934
nasifimtiazohi <nasifimtiaz88@gmail.com>
2019-03-20 12:58:08 -0400

    add query for invalidate alerts before first_deteced

--- execute_last_sql_statement_kodi.py
+++ execute_last_sql_statement_kodi.py
@@ -8,7 +8,15 @@ connection = pymysql.connect(host='localhost',
                              autocommit=True)
 
 queries=[
+    #initiate Kodi on project database
     "insert into projects values(null,'Kodi','https://github.com/xbmc/xbmc','git://github.com/xbmc/xbmc.git','2012-08-28','2019-03-16');"
+    #invalidate the alerts before first detected
+    "update alerts \
+    set is_invalid=1 \
+    where first_detected  < \
+                            (select start_date \
+                            from projects \
+                            where stream='Kodi')"
 ]
 
 with connection.cursor() as cursor:
