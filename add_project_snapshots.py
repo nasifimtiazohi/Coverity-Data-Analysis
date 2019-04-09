e59c4db4e9aee8450f4f7b63f70e95f65e59d2dc
nasifimtiazohi <nasifimtiaz88@gmail.com>
2019-04-09 10:55:30 -0400

    updated project snapshot add

--- add_project_snapshots.py
+++ add_project_snapshots.py
@@ -15,7 +15,7 @@ def is_number(n):
 
 connection = pymysql.connect(host='localhost',
                              user='root',
-                             db='890coverity',
+                             db='coverityscan',
                              charset='utf8mb4',
                              cursorclass=pymysql.cursors.DictCursor,
                              autocommit=True)
