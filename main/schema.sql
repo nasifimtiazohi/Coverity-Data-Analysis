28fba1fbb45d6803a2bca87a5d60c4040e7bd89b
nasifimtiazohi <nasifimtiaz88@gmail.com>
2020-06-19 22:27:47 -0400

    design of V2 database

--- main/schema.sql
+++ main/schema.sql
@@ -1,190 +1,262 @@
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
-SET @OLD_TIME_ZONE=@@session.time_zone;
+create database coverityscanV2;
+use coverityscanV2;
 
-DROP SCHEMA IF EXISTS `soverityscan_sandbox` ;
-CREATE SCHEMA IF NOT EXISTS `soverityscan_sandbox` DEFAULT CHARACTER SET utf8;
-USE `soverityscan_sandbox` ;
+create table alert_type
+(
+    id       int auto_increment
+        primary key,
+    type     varchar(255) null,
+    impact   varchar(255) null,
+    category varchar(255) null,
+    constraint alert_type_pk
+        unique (type)
+)
+    charset = utf8;
 
-CREATE TABLE `projects` (
-  `idprojects` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(255) NULL,
-  `repository_url_on_coverity` VARCHAR(255) NULL COMMENT "repository url as listed on Coverity Scan",
-  `github_url` VARCHAR(255) NULL,
-  `stream_count` INT NOT NULL,
-  `branch_aka_stream` VARCHAR(255) NULL,  -- if we find two streams for any project (unlikely) we have to make use of this column
-  `start_date` DATETIME NULL,
-  `end_date` DATETIME NULL,
-  UNIQUE INDEX `unique_stream` (`name`,`branch_aka_stream`),
-  PRIMARY KEY (`idprojects`));
+create table invalid_alert_category
+(
+    id       tinyint      not null
+        primary key,
+    category varchar(255) not null
+);
 
-CREATE TABLE `snapshots` (
-`idsnapshots` INT NOT NULL,
-`stream` VARCHAR(255) NOT NULL,
-`date` DATETIME NULL,
-`description` LONGTEXT NULL,
-`total_detected` INT NULL,
-`newly_detected` INT NULL,
-`newly_eliminated` INT NULL,
-`analysis_time` TIME NULL,
-`lines_of_code` INT NULL,
-`code_version_date` DATETIME NULL,
-`file_count` INT NULL,
-`function_count` INT NULL,
-`version` LONGTEXT NULL,
-`blankLinesCount` INT NULL,
-`buildTime` TIME NULL,
-`commentLinesCount` INT NULL,
-`hasAnalysisSummaries` LONGTEXT NULL,
-`target` LONGTEXT NULL,
-`last_snapshot` INT COMMENT "keeping track of last snapshot within our data for this stream",
-PRIMARY KEY (`idsnapshots`,`stream`));
+create table project
+(
+    id         int auto_increment
+        primary key,
+    name       varchar(255) null,
+    url        varchar(255) null comment 'repository url as listed on Coverity Scan',
+    github_url varchar(255) null,
+    branch     varchar(255) null,
+    start_date datetime     null,
+    end_date   datetime     null,
+    constraint unique_stream
+        unique (name, branch)
+)
+    charset = utf8;
 
+create table commit
+(
+    id                   int auto_increment
+        primary key,
+    project_id           int           not null,
+    sha                  varchar(255)  null,
+    author               varchar(4095) null,
+    author_email         varchar(4095) null,
+    author_date          datetime      null,
+    committer            varchar(4095) null,
+    committer_email      varchar(4095) null,
+    commit_date          datetime      null,
+    message              longtext      null,
+    affected_files_count int           null,
+    net_lines_added      int           null,
+    net_lines_removed    int           null,
+    is_merged            varchar(255)  null,
+    constraint commit_pk
+        unique (sha, project_id),
+    constraint sha
+        unique (sha),
+    constraint commit_project_id_fk
+        foreign key (project_id) references project (id)
+            on update cascade
+)
+    charset = utf8;
 
+create table file
+(
+    id                   int auto_increment
+        primary key,
+    project_id           int          not null,
+    filepath_on_coverity varchar(512) null comment 'begins with /',
+    is_processed         tinyint      null,
+    constraint file_pk
+        unique (project_id, filepath_on_coverity),
+    constraint file_project_id_fk
+        foreign key (project_id) references project (id)
+            on update cascade
+)
+    charset = utf8;
 
-CREATE TABLE `bug_types` (
-`idbug_types` INT NOT NULL AUTO_INCREMENT,
-`type` VARCHAR(255) NULL,
-`impact` VARCHAR(255) NULL,
-`category` VARCHAR(255) NULL,
-PRIMARY KEY (`idbug_types`));
+create table alert
+(
+    id                  int auto_increment
+        primary key,
+    cid                 int           not null,
+    project_id          int           not null,
+    alert_type_id       int           not null,
+    status              varchar(255)  null,
+    first_detected      date          null,
+    owner               varchar(1023) null,
+    classification      varchar(1023) null,
+    severity            varchar(1023) null,
+    action              longtext      null,
+    component           longtext      null,
+    file_id             int           null,
+    function            longtext      null,
+    checker             longtext      null,
+    count               int           null,
+    CWE                 int           null,
+    ext_ref             longtext      null,
+    first_snapshot_id   int           null,
+    function_merge_name longtext      null,
+    issue_kind          varchar(255)  null,
+    language            varchar(255)  null,
+    last_snapshot_id    int           null,
+    last_triaged        datetime      null,
+    legacy              varchar(255)  null,
+    merge_extra         longtext      null,
+    merge_key           longtext      null,
+    owner_name          varchar(1023) null,
+    is_invalid          tinyint       null,
+    constraint alert_pk
+        unique (cid, project_id),
+    constraint alert_alert_type_id_fk
+        foreign key (alert_type_id) references alert_type (id)
+            on update cascade,
+    constraint alert_file_id_fk
+        foreign key (file_id) references file (id),
+    constraint alert_invalid_alert_category_id_fk
+        foreign key (is_invalid) references invalid_alert_category (id)
+            on update cascade,
+    constraint alert_project_id_fk
+        foreign key (project_id) references project (id)
+            on update cascade
+)
+    charset = utf8;
 
-CREATE TABLE `files` (
-  -- how to handle branching? not current concern
-`idfiles` INT NOT NULL AUTO_INCREMENT,
-`project` VARCHAR(255) NULL,
-`filepath_on_coverity` VARCHAR(4095) NULL COMMENT "begins with /",
-PRIMARY KEY (`idfiles`));
+create table actionability
+(
+    alert_id                 int         not null
+        primary key,
+    actionability            int         null,
+    marked_bug               varchar(45) null,
+    file_activity_around_fix varchar(45) null,
+    single_fix_commit        int         null,
+    fix_commits              longtext    null,
+    file_deleted             varchar(45) null,
+    delete_commit            int         null,
+    file_renamed             varchar(45) null,
+    rename_commit            int         null,
+    transfered_alert_id      longtext    null,
+    suppression              varchar(45) null,
+    suppress_commit          int         null,
+    suppress_keyword         varchar(45) null,
+    constraint actionability_alert_id_fk
+        foreign key (alert_id) references alert (id)
+            on update cascade,
+    constraint actionability_commit_id_fk
+        foreign key (single_fix_commit) references commit (id)
+            on update cascade
+);
 
-CREATE TABLE `filecommits` (
-`idfilecommits` INT NOT NULL AUTO_INCREMENT,
-`file_id` INT NULL,
-`commit_id` INT NULL,
-`change_type` VARCHAR(255) NULL COMMENT "how the file is changed",
-`lines_added` INT NULL,
-`lines_removed` INT NULL,
-PRIMARY KEY (`idfilecommits`))
-COMMENT="this table keep track of unique file and commit pairs for ease of analysis";
+create table filecommit
+(
+    id            int auto_increment
+        primary key,
+    file_id       int          null,
+    commit_id     int          null,
+    change_type   varchar(255) null comment 'how the file is changed',
+    lines_added   int          null,
+    lines_removed int          null,
+    constraint filecommit_commit_id_fk
+        foreign key (commit_id) references commit (id)
+            on update cascade,
+    constraint filecommit_file_id_fk
+        foreign key (file_id) references file (id)
+            on update cascade
+)
+    comment 'this table keep track of unique file and commit pairs for ease of analysis' charset = utf8;
 
-CREATE TABLE `soverityscan_sandbox`.`commits` (
-  -- I expect more info to come here. e.g. parent?
-  `idcommits` INT NOT NULL AUTO_INCREMENT,
-  `sha` VARCHAR(1023) NULL UNIQUE,
-  `author` VARCHAR(4095) NULL,
-  `author_email` VARCHAR(4095) NULL,
-  `author_date` DATETIME NULL,
-  `committer` VARCHAR(4095) NULL,
-  `committer_email` VARCHAR(4095) NULL,
-  `commit_date` DATETIME NULL,
-  `message` LONGTEXT NULL,
-  `affected_files_count` INT NULL,
-  `net_lines_added` INT NULL,
-  `net_lines_removed` INT NULL, 
-  `is_merged` VARCHAR(255),
-  `project` VARCHAR(255) NOT NULL,
-  `merge_date` DATETIME NOT NULL,
-  PRIMARY KEY (`idcommits`));
+create table diff
+(
+    id             int auto_increment
+        primary key,
+    filecommit_id  int      not null,
+    old_start_line int      null,
+    old_count      int      null,
+    new_start_line int      null,
+    new_count      int      null,
+    content        longtext null,
+    constraint diff_filecommits_idfilecommits_fk
+        foreign key (filecommit_id) references filecommit (id)
+            on update cascade
+)
+    charset = utf8;
 
-CREATE TABLE `soverityscan_sandbox`.`commit_parents` (
-  `commit_id` INT NOT NULL,
-  `parent_hash` VARCHAR(4095) NULL,
-  `parent_commit_id` INT NULL);
+create table fix_complexity
+(
+    alert_id              int   not null
+        primary key,
+    commit_id             int   not null,
+    file_count            float null,
+    net_loc_change        float null,
+    infile_loc_change     float null,
+    net_logical_change    float null,
+    infile_logical_change float null,
+    total_fixed_alerts    int   null,
+    infile_fixed_alerts   int   null,
+    constraint fix_complexity_alerts_idalerts_fk
+        foreign key (alert_id) references alert (id)
+            on update cascade,
+    constraint fix_complexity_commits_idcommits_fk
+        foreign key (commit_id) references commit (id)
+            on update cascade
+);
 
-CREATE TABLE `soverityscan_sandbox`.`diffs` (
-  `iddiffs` INT NOT NULL AUTO_INCREMENT,
-  `filecommit_id` INT NOT NULL,
-  `old_start_line` INT NULL,
-  `old_count` INT NULL,
-  `new_start_line` INT NULL,
-  `new_count` INT NULL,
-  `content` LONGTEXT NULL,
-  PRIMARY KEY (`iddiffs`));
+create index fix_complexity_commit_id_index
+    on fix_complexity (commit_id);
 
-CREATE TABLE `soverityscan_sandbox`.`parsed_diff` (
-  `diff_id` INT NOT NULL,
-  `change_type` VARCHAR(255) NULL,
-  `line_number` INT NULL,
-  `code` LONGTEXT NULL);
+create table merge_date
+(
+    commit_id  int      not null
+        primary key,
+    merge_date datetime null,
+    constraint merge_date_commits_idcommits_fk
+        foreign key (commit_id) references commit (id)
+            on update cascade
+);
 
-CREATE TABLE `alerts` (
-`idalerts` INT NOT NULL AUTO_INCREMENT,
-`cid` INT NOT NULL,
-`stream` VARCHAR(255) NOT NULL,
-`bug_type` VARCHAR(255) NULL,
-`status` VARCHAR(255) NULL,
-`first_detected` DATE NULL,
-`owner` VARCHAR(1023) NULL,
-`classification` VARCHAR(1023) NULL,
-`severity` VARCHAR(1023) NULL,
-`action` LONGTEXT NULL,
-`component` LONGTEXT NULL,
-`file_id` INT NULL,
-`function` LONGTEXT NULL,
-`checker` LONGTEXT NULL,
-`count` INT NULL,
-`CWE` INT NULL,
-`ext_ref` LONGTEXT NULL,
-`first_snapshot_id` INT NULL,
-`function_merge_name` LONGTEXT NULL,
-`issue_kind` VARCHAR(255) NULL,
-`language` VARCHAR(255) NULL,
-`last_snapshot_id` INT NULL,
-`last_triaged` DATETIME NULL,
-`legacy` VARCHAR(255) NULL,
-`merge_extra` LONGTEXT NULL,
-`merge_key` LONGTEXT NULL,
-`owner_name` VARCHAR(1023) NULL,
--- what is the conditions for is_invalid??
-`is_invalid` TINYINT NULL,
-UNIQUE `unique_index`(`cid`,`stream`),
-PRIMARY KEY (`idalerts`));
+create table occurrence
+(
+    alert_id       int           not null,
+    cid            int           not null,
+    event_id       int           not null,
+    short_filename varchar(4095) not null,
+    file_id        int           null,
+    line_number    int           null,
+    is_defect_line tinyint       null,
+    primary key (alert_id, event_id),
+    constraint occurrence_alert_id_fk
+        foreign key (alert_id) references alert (id)
+            on update cascade
+)
+    charset = utf8;
 
-CREATE TABLE `soverityscan_sandbox`.`occurrences` (
-`alert_id` INT NOT NULL,
-`cid` INT NOT NULL,
-`event_id` INT NOT NULL,
-`short_filename` VARCHAR(4095) NOT NULL,
-`file_id` INT NULL,
-`line_number` INT NULL,
-`is_defect_line` TINYINT,
-PRIMARY KEY (`alert_id`, `event_id`));
-
-
-CREATE TABLE `soverityscan_sandbox`.`fix_complexity` (
-  `commit_id` INT NOT NULL,
-  `alert_id` INT NOT NULL,
-  `file_count` FLOAT NULL,
-  `net_affected_function` FLOAT NULL,
-  `infile_affected_function` FLOAT NULL,
-  `net_loc_change` FLOAT NULL,
-  `infile_loc_change` FLOAT NULL,
-  `net_logical_change` FLOAT NULL,
-  `infile_logical_change` FLOAT NULL,
-  `total_fixed_alerts` INT NULL,
- `infile_fixed_alerts` INT NULL,
-  PRIMARY KEY (`commit_id`, `alert_id`));
-
-CREATE TABLE `soverityscan_sandbox`.`actionability` (
-  `alert_id` INT NOT NULL,
-  `actionability` INT NULL,
-  `marked_bug` VARCHAR(45) NULL,
-  `file_activity_around_fix` VARCHAR(45) NULL,
-  `single_fix_commit` INT NULL,
-  `fix_commits` LONGTEXT NULL,
-  `file_deleted` VARCHAR(45) NULL,
-  `delete_commit` INT NULL  ,
-  `file_renamed` VARCHAR(45) NULL,
-  `rename_commit` INT NULL  ,
-  `transfered_alert_id` LONGTEXT NULL,
-  `suppression` VARCHAR(45) NULL,
-  `suppress_commit` INT NULL,
-  `suppress_keyword` VARCHAR(45) NULL,
-  PRIMARY KEY (`alert_id`));
-
-CREATE TABLE `soverityscan_sandbox`.`merge_date` (
-  `commit_id` INT NOT NULL,
-  `merge_date` DATETIME NULL,
-  PRIMARY KEY (`commit_id`));
-
+create table snapshot
+(
+    id                     int      not null,
+    project_id             int      not null,
+    date                   datetime null,
+    description            longtext null,
+    total_detected         int      null,
+    newly_detected         int      null,
+    newly_eliminated       int      null,
+    analysis_time          time     null,
+    lines_of_code          int      null,
+    code_version_date      datetime null,
+    file_count             int      null,
+    function_count         int      null,
+    version                longtext null,
+    blank_lines            int      null,
+    build_time             time     null,
+    comment_lines          int      null,
+    has_analysis_summaries longtext null,
+    target                 longtext null,
+    last_snapshot          int      null comment 'keeping track of last snapshot within our data for this stream',
+    primary key (id, project_id),
+    constraint snapshot_project_id_fk
+        foreign key (project_id) references project (id)
+            on update cascade
+)
+    charset = utf8;
 
